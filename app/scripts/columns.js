c2a0f4625d95e4462abde3661c86bd2ab668d7ef
Eric Tsang <tsang.eric@gmail.com>
2015-11-01 00:00:29 +0800

    bug discovered on non square imgs

--- app/scripts/columns.js
+++ app/scripts/columns.js
@@ -23,7 +23,7 @@ $(function(){
       var count = countAssets(source);
       console.log('start', source, count);
 
-      preConfigWidths(source, count - 1);
+      // preConfigWidths(source, count - 1);
       $(this).sortable('refreshPositions');
     },
     change: function(event, ui) {
@@ -104,16 +104,16 @@ function countAssets(id) {
   return assets.length;
 }
 
-function preConfigWidths(id, count) {
-  var fudge = 1;
-  var assets = $('#' + id + ' .collection img');
-  var collectionWidth = $('#' + id + ' .wrapper').width();
-  var normalizedWidth = collectionWidth / count - fudge;
-  assets.each(function (key, value) {
-    value.width = normalizedWidth;
-    $(value).animate({ width: normalizedWidth }, 80);
-  });
-}
+// function preConfigWidths(id, count) {
+//   var fudge = 1;
+//   var assets = $('#' + id + ' .collection img');
+//   var collectionWidth = $('#' + id + ' .wrapper').width();
+//   var normalizedWidth = collectionWidth / count - fudge;
+//   assets.each(function (key, value) {
+//     value.width = normalizedWidth;
+//     $(value).animate({ width: normalizedWidth }, 80);
+//   });
+// }
 
 function hintOn(id) {
   $(id).addClass('hint');
